498183b195ec521a93dee64ab8738b56cc688b0b
pranith124 <praneeth2.4k@gmail.com>
2025-11-17 01:46:27 +0530

    feat: day 4 learning aync await promises callback hell

--- week-2/promise-class.js
+++ week-2/promise-class.js
@@ -10,11 +10,81 @@
 
 // setTimeoutPromisified(2000).then(callback);
 
-function promisifiedcallback(resolve){
-    setTimeout(resolve,2000);
+// function promisifiedcallback(resolve){
+//     setTimeout(resolve,2000);
+// }
+
+// function callback2(){
+//     console.log("2 seconds paased ");
+// }
+//  promisifiedcallback(callback2);
+
+//call back hell
+// function callback(){
+//     console.log("function is executed");
+// }
+
+// setTimeout(function(){
+//     console.log("Hi");
+//     setTimeout(function(){
+//         console.log("hello");
+//         setTimeout(function(){
+//             console.log("hai hello");
+//         },2000);
+//     },3000);
+// },1000);
+
+// promisified version call back hell chaining
+// function setTimeoutPromisified(duration){
+//     return new Promise(function (resolve){
+//         setTimeout(resolve,duration);
+//     });
+// }
+
+// setTimeoutPromisified(1000).then(
+//     function(){
+//         console.log("Hi");
+//         return setTimeoutPromisified(2000);
+// }).then(
+//     function(){
+//         console.log("hai");
+//         return setTimeoutPromisified(3000);
+// }).then(
+//     function(){
+//         console.log("hello hai");
+//     }
+// )
+
+// function setTimeoutPromisified(duration){
+//     return new Promise(function(resolve){
+//         setTimeout(resolve,duration);
+//     });
+// }
+
+// function callback(){
+//     console.log("promise is executed after 1 sec");
+// };
+
+// function callback2(){
+//     console.log("promise is executed after first function");
+// }
+
+// setTimeoutPromisified(2000).then(callback).then(callback2);
+
+function setTimeoutPromisified(duration){
+    return new Promise(function(resolve,){
+        setTimeout(resolve,duration);
+    })
 }
 
-function callback2(){
-    console.log("2 seconds paased ");
+async function solve() {
+    await setTimeoutPromisified(1000);
+    console.log("hii");
+    await setTimeoutPromisified(2000);
+    console.log("hai");
+    await setTimeoutPromisified(3000);
+    console.log("hello hai");
+    
 }
- promisifiedcallback(callback2);
+
+solve();
