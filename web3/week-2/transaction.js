5aa7930f535f61fbc7e63796051a99847ddae765
pranith124 <praneeth2.4k@gmail.com>
2025-11-21 00:39:54 +0530

    day-5 learning content

--- web3/week-2/transaction.js
+++ web3/week-2/transaction.js
@@ -1,5 +1,5 @@
 import { keygen } from "@noble/ed25519";
-import { Keypair, PublicKey } from "@solana/web3.js";
+import { EpochSchedule, Keypair, PublicKey } from "@solana/web3.js";
 import nacl from "tweetnacl";
 
 const keypair = Keypair.generate();
@@ -15,6 +15,21 @@ const Signature = nacl.sign.detached(message,privateKey);
 
 const result = nacl.sign.detached.verify(message,Signature,keypair.publicKey.toBytes());
 
-const 
+
 console.log(result);
+
+
+// hd wallets
+// generating the seed phrase from the seed phrase we can creare as many wallets as we want and we can back up this wallets and we can login this wallets in differnt wallets (phantom,webpack)
+
+// Context-Hashing(SHA-256),Encryption(EDDSA,ECDSA),Public and Private keys,HD Wallets
+// keccak-256
+// Frontend vs backend,Http Servers
+// RPC, JSON-RPC
+// Wei, Lamports
+// RPC server
+// Common RPC calls on Solana
+// Common RPC calls on ETH
+// Creating a web based wallet
+
 
